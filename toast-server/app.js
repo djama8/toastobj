5eb6e0758b38db41e4a7ae9ff7b5d3ef4f298004
djama <8djama8@mail.ru>
2024-02-14 12:14:15 +0300

    first commit

---
+++ toast-server/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extented: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send({
+        message: "Успешное подключение к серверу"
+    }
+    );
+})
+
+
+app.post('/login', (req, res) => {
+
+    const user ={
+        login:'admin',
+        password: '1010'
+    }
+
+    if (req.body.login != user.login){
+        res.status(404).send({message: 'Пользователь не найден'})
+    }
+    if (req.body.login === user.login && req.body.password != user.password){
+        res.status(403).send({message: 'Неверный пароль'})
+    }
+    if (req.body.login === user.login && req.body.password === user.password){
+        res.send({message: 'Успешно'});
+    }
+    console.log(req.body)
+})
+app.listen(3001,()=>{
+    console.log("Сервер запущен. Порт: 3001");
+})
